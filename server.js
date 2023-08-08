c175d3f2215543bfc861f580748cf21670fc2655
Ronin1702 <kchen@me.com>
2023-08-08 01:59:11 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,47 +1,44 @@
-require('dotenv').config();
-
 // initialize variables
 const path = require('path'),
-      express = require('express'),
-      session = require('express-session'),
-      expressHandlebars = require('express-handlebars'),
-      routes = require('./controllers'),
-      helpers = require('./utils/helpers'),
-      sequelize = require('./config/connection'),
-      SequelizeStore = require('connect-session-sequelize')(session.Store),
-      app = express(),
-      PORT = process.env.PORT || 3000,
-      handlebars = expressHandlebars.create({ helpers }),
-      sesh = {
-        // secret used to sign the session ID cookie
-        secret: process.env.SECRET, 
-        cookie: {
-          // session expiration in milliseconds (30 minutes)
-          maxAge: 1800000, 
-        },
-        // forces the session to be saved back to the session store, even if it hasn't been modified
-        resave: false, 
-        // forces a session that is "uninitialized" to be saved to the store
-        saveUninitialized: true, 
-        store: new SequelizeStore({
-          // sequelize database connection
-          db: sequelize 
-        })
-      };
+  express = require('express'),
+  session = require('express-session'),
+  expressHandlebars = require('express-handlebars'),
+  routes = require('./controllers'),
+  helpers = require('./utils/helpers'),
+  sequelize = require('./config/connection'),
+  SequelizeStore = require('connect-session-sequelize')(session.Store),
+  app = express(),
+  PORT = process.env.PORT || 3000,
+  handlebars = expressHandlebars.create({ helpers }),
+  sesh = {
+    // secret used to sign the session ID cookie
+    secret: process.env.SECRET,
+    cookie: {
+      // session expiration in milliseconds (15 minutes)
+      maxAge: 900000,
+    },
+    // forces the session to be saved back to the session store, even if it hasn't been modified
+    resave: false,
+    // forces a session that is "uninitialized" to be saved to the store
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      // sequelize database connection
+      db: sequelize
+    })
+  };
+// Use the session middleware
+app.use(session(sesh));
+// configure the view engine to use Handlebars templates
+app.engine('handlebars', handlebars.engine);
+// set the default view engine to handlebars
+app.set('view engine', 'handlebars');
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Use the session middleware
-app.use(session(sesh));
-// configure the view engine to use Handlebars templates
-app.engine('handlebars', handlebars.engine); 
-// set the default view engine to handlebars
-app.set('view engine', 'handlebars'); 
-
 // serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Register routes
 app.use(routes);
