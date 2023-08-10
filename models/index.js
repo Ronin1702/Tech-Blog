f20f363fd1ba852b2f1077095b63d6501b30b53e
Ronin1702 <kchen@me.com>
2023-08-10 02:16:04 -0400

    added comments to code

--- models/index.js
+++ models/index.js
@@ -5,28 +5,34 @@ const Users = require('./User')
 
 // Users: one-to-many (user has many posts)
 Users.hasMany(Post, {
-  foreignKey: 'user_id' // foreign key in post model referencing user_id in users model
+  // foreign key in post model referencing user_id in users model
+  foreignKey: 'user_id'
 });
 
 // Users: one-to-many (user has many comments)
 Users.hasMany(Comment, {
-  foreignKey: 'user_id' // foreign key in comment model referencing user_id in users model
+  // foreign key in comment model referencing user_id in users model
+  foreignKey: 'user_id'
 });
 
 // Post: many-to-one (post belongs to a user)
 Post.belongsTo(Users, {
-  foreignKey: 'user_id' // foreign key in post model referencing user_id in users model
+  // foreign key in post model referencing user_id in users model
+  foreignKey: 'user_id'
 });
 
 // Post: one-to-many (post has many comments)
 Post.hasMany(Comment, {
-  foreignKey: 'post_id', // Foreign key in Comment model referencing post_id in Post model
-  onDelete: 'CASCADE' // when a post is deleted, all associated comments are also deleted
+  // Foreign key in Comment model referencing post_id in Post model
+  foreignKey: 'post_id',
+  // when a post is deleted, all associated comments are also deleted
+  onDelete: 'CASCADE'
 });
 
 // Comment: many-to-one (comment belongs to a user)
 Comment.belongsTo(Users, {
-  foreignKey: 'user_id' // foreign key in comment model referencing user_id in users model
+  // foreign key in comment model referencing user_id in users model
+  foreignKey: 'user_id'
 });
 
 // Comment: many-to-one (comment belongs to a post)
